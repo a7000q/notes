4858f929fcd3d61ca19dc4852edd12e28be613df
rail <a7000q@gmail.com>
2016-09-08 16:43:05 +0200

    first commit

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=notes_base',
     'username' => 'root',
-    'password' => '',
+    'password' => 'Rail05071989!',
     'charset' => 'utf8',
 ];
